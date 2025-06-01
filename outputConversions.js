5af231cdfdde6b828971c71d1c57fbb4668d88fb
satyams2407 <satyams2407@gmail.com>
2025-06-01 12:05:36 +0000

    output conversions

---
+++ outputConversions.js
@@ -0,0 +1,19 @@
+console.log(1+1); // 2
+console.log("a" + "b"); // ab
+console.log("a" + 1); // a1
+console.log(1 + "a"); // 1a
+console.log(typeof(1 + 1 + "a")); // string
+console.log(1 + 1 + "a"); // 2a
+console.log("a" + "a" + 1); // aa1
+console.log("a" + 1 + "b" + 2); // a1b2
+
+
+
+let a = 10;
+let c = ++a;
+let b = a++;
+
+console.log(b);
+console.log(c);
+console.log(a);
+
