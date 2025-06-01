0a4b4d855dd1cd459f23cb8a154916c6db8d848a
satyams2407 <satyams2407@gmail.com>
2025-06-01 10:36:37 +0000

    datatypes amd conversion

---
+++ datatypes.js
@@ -0,0 +1,25 @@
+const score = "33"
+console.log(typeof(score))
+
+const value = Number(score)
+console.log(typeof(score))
+console.log(value);
+
+
+//conversion   Boolean , String , Number
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// "true" => NaN
+
+const num = 1
+const isBool = Boolean(num)
+console.log(isBool);
+
+// 1 => true ; 0 => false; "" => false; "abc" => true
+
+
+let a;
+const b = String(a)
+console.log(typeof(b));
